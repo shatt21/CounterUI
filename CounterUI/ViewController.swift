f49a675366d401d18842ae311c16f6716d21f554
shatt21 <poller2224@yandex.ru>
2023-08-30 19:37:20 +0300

    Добавил кнопку счетчика

--- CounterUI/ViewController.swift
+++ CounterUI/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBAction func counterButton(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = String(counter)
         // Do any additional setup after loading the view.
     }
 
